3d838dbe690a67e8d838e9dc19af11933e35781f
sinaziaee <sinaziaee99@gmail.com>
2021-01-26 20:43:02 +0330

    All the fixes

--- lib/constants.dart
+++ lib/constants.dart
@@ -28,7 +28,7 @@ const List<BoxShadow> shadow = [
 ];
 
 final kShadowColor = Color(0xFFD3D3D3).withOpacity(.84);
-String baseUrl = 'http://danibazi9.pythonanywhere.com';
+String baseUrl = 'http://172.17.3.157';
 screenAwareSize(int size, BuildContext context) {
   double baseHeight = 640.0;
   return size * MediaQuery.of(context).size.height / baseHeight;
